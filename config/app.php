398bcb3cc3c874c515ea85d33245389a357f221b
151610273-smkassalaambandung <151610273-smkassalaambandung>
2017-06-13 07:54:24 +0700

    belajar larapus

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'My Application',
+    'name' => 'Larapus',
 
     /*
     |--------------------------------------------------------------------------
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
 
         //
 
@@ -225,6 +226,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' =>Collective\Html\HtmlFacade::class,
+        
     ],
 
 ];
--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'admin@larapus.com',
+        'name' => 'Admin Larapus',
     ],
 
     /*
